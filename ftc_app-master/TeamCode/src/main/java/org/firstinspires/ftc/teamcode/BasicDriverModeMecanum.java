26623e276f0f920680a2914cc80d0ff4b0bc78af
Griffin Walraven <griffin.walraven@gmail.com>
2020-10-24 18:12:13 -0700

    mecanum stuff

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriverModeMecanum.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicDriverModeMecanum.java
@@ -9,14 +9,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Basic Drive Mode", group="Iterative Opmode")
+@TeleOp(name="Basic Drive Mode Mecanum", group="Iterative Opmode")
 @Disabled
 public class BasicOpMode_Iterative extends OpMode
 {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
+    private DcMotor hubOneMotorOne = null;
+    private DcMotor hubOneMotorTwo = null;
+    private DcMotor hubOneMotorThree = null;
+    private DcMotor hubOneMotorFour = null;
 
     // Code to run ONCE when the driver hits INIT
     @Override
@@ -24,10 +26,10 @@ public class BasicOpMode_Iterative extends OpMode
         telemetry.addData("Status", "Initialized");
 
         // Initialize the hardware variables.
+        hubOneMotorZero = hardwareMap.get(DcMotor.class, "hubOneMotorZero");
         hubOneMotorOne = hardwareMap.get(DcMotor.class, "hubOneMotorOne");
         hubOneMotorTwo = hardwareMap.get(DcMotor.class, "hubOneMotorTwo");
         hubOneMotorThree = hardwareMap.get(DcMotor.class, "hubOneMotorThree");
-        hubOneMotorFour = hardwareMap.get(DcMotor.class, "hubOneMotorFour");
 
         // Make sure motors are going in the right diretion
 
@@ -50,16 +52,33 @@ public class BasicOpMode_Iterative extends OpMode
     @Override
     public void loop() {
         // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+        double zeroPower;
+        double onePower;
+        double twoPower;
+        double threePower;
 
+        // Set the power for mecanum wheels
+        zeroPower = gamepad1.left_stick_y - gamepad1.left_stick_x;
+        onePower = gamepad1.left_stick_y + gamepad1.left_stick_x;
+        twoPower = gamepad1.left_stick_y + gamepad1.left_stick_x;
+        threePower = gamepad1.left_stick_y - gamepad1.left_stick_x;
+
+        // Clip the power to be within -1 to 1 bounds
+        zeroPower  = Range.clip(zeroPower, -1, 1);
+        onePower   = Range.clip(onePower, -1, 1);
+        twoPower   = Range.clip(twoPower, -1, 1);
+        threePower = Range.clip(threePower, -1, 1);
+        
     	// Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
+    	hubOneMotorZero.setPower(zeroPower);
+    	hubOneMotorOne.setPower(onePower);
+    	hubOneMotorTwo.setPower(twoPower);
+    	hubOneMotorThree.setPower(threePower);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Motors", "zero (%.2f), two (%.2f), three (%.2f), four (%.2f)",
+                          hubOneMotorZero, hubOneMotoOne, hubOneMotorTwo, hubOneMotorThree);
     }
 
     /*
